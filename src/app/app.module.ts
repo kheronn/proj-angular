d57f2801e96d8ec2d3e35124d92bc4a8efed78d4
Kheronn <kheronn@gmail.com>
2019-03-14 11:44:22 -0300

    Update lib localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
+import { ContatoService } from './services/contato.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContatoFormComponent } from './contato-form/contato-form.component';
 import { FormsModule } from '@angular/forms';
 import { LocalStorageModule } from 'angular-2-local-storage';
-import {MatTableModule} from '@angular/material/table';
 import { ContatoListComponent } from './contato-list/contato-list.component';
 
 @NgModule({
@@ -17,16 +16,14 @@ import { ContatoListComponent } from './contato-list/contato-list.component';
     HomeComponent,
     ContatoFormComponent,
     ContatoListComponent,
-    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatTableModule,
-    BrowserAnimationsModule,
     LocalStorageModule.forRoot({
-      storageType : 'localStorage'
+      storageType: 'localStorage'
     })
   ],
   providers: [],
