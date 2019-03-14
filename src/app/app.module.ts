296d1c2f54ed7918e13534e235b5847f017ee0e0
Kheronn <kheronn@gmail.com>
2019-03-14 13:03:02 -0300

    Remove imports not use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
-import { ContatoService } from './services/contato.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
